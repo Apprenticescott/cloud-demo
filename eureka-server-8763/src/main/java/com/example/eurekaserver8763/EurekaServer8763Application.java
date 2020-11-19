0531ef95c30a34406e1bf0d8ff84c07671d23e3e
SmallBulb <1765119304@qq.com>
2020-11-19 10:22:50 +0800

    springcloud-demo

---
+++ eureka-server-8763/src/main/java/com/example/eurekaserver8763/EurekaServer8763Application.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserver8763;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer8763Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer8763Application.class, args);
+    }
+
+}
--- feign-9001/src/main/java/com/example/feign9001/controller/HelloController.java
+++ feign-9001/src/main/java/com/example/feign9001/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.example.feign9001.controller;
 
 import com.example.feign9001.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +14,8 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
-    @Value("${spring.version}")
-    String version;
-
     @GetMapping("/hello")
     public String hello() {
-        return helloService.Hello() + version;
+        return helloService.Hello();
     }
 }
